574c068d6975fcec8e1945253f3de90c0cf3cbb9
linda <lindaliz5995@gmail.com>
2021-06-27 14:54:20 +0530

    minor improvements

--- Backend/app.js
+++ Backend/app.js
@@ -47,7 +47,7 @@ io.on("connection", (socket) => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit("message", formatMessage(bot, "Welcome to ChatCord!"));
+    socket.emit("message", formatMessage(bot, "Welcome to ChatStation!"));
 
     // Broadcast when a user connects
     socket.broadcast
